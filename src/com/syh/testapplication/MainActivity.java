1a56dc2b56ab8d6cbac3cfcdab8ae4c1b4e4c589
lupeng.kang <lupeng.kang@renren-inc.com>
2017-01-16 16:02:45 +0800

    添加备注&修改图片大小

--- src/com/syh/testapplication/MainActivity.java
+++ src/com/syh/testapplication/MainActivity.java
@@ -1,25 +1,6 @@
 package com.syh.testapplication;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.app.PendingIntent.CanceledException;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.ContentObserver;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.Bitmap.Config;
 import android.graphics.BitmapFactory;
@@ -29,23 +10,17 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.os.SystemClock;
-import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
-import android.telephony.SmsManager;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import com.syh.wechatImageClipping.R;
+
+/**
+ * 这个demo有个问题，就是右侧的那个角的下边框展示不出来，
+ * 解决方案是让UI提供两个点九图：bitmap_bg_1和 bitmap_bg_2
+ * bitmap_bg_1尖角以上区域的高度比bitmap_bg_2尖角以上区域的高度小10px
+ *
+ */
 public class MainActivity extends Activity {
 
 	private ImageView iv_image;
@@ -62,10 +37,11 @@ public class MainActivity extends Activity {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				Bitmap bitmap_bg = BitmapFactory.decodeResource(getResources(), R.drawable.chat_adapter_to_bg);
-				Bitmap bitmap_in = BitmapFactory.decodeResource(getResources(), R.drawable.aa);
-				final Bitmap bp = getRoundCornerImage(bitmap_bg, bitmap_in);
-				final Bitmap bp2 = getShardImage(bitmap_bg, bp);
+				Bitmap bitmap_bg_1 = BitmapFactory.decodeResource(getResources(), R.drawable.chat_adapter_to_bg);
+				Bitmap bitmap_bg_2 = BitmapFactory.decodeResource(getResources(), R.drawable.chat_adapter_to_bg);
+				Bitmap bitmap_in = BitmapFactory.decodeResource(getResources(), R.drawable.zhaoliying);
+				final Bitmap bp = getRoundCornerImage(bitmap_bg_1, bitmap_in);
+				final Bitmap bp2 = getShardImage(bitmap_bg_2, bp);
 				runOnUiThread(new Runnable() {
 					@Override
 					public void run() {
@@ -78,28 +54,37 @@ public class MainActivity extends Activity {
 	
 	public static Bitmap getRoundCornerImage(Bitmap bitmap_bg,Bitmap bitmap_in)
 	{
-		Bitmap roundConcerImage = Bitmap.createBitmap(500,500, Config.ARGB_8888);
+		//创建一个宽高都是500的bitMap
+		Bitmap roundConcerImage = Bitmap.createBitmap(790,790, Config.ARGB_8888);
+		//用canvas绘制bitMap
 		Canvas canvas = new Canvas(roundConcerImage);
+		//创建一个画笔
 		Paint paint = new Paint();
-		Rect rect = new Rect(0,0,500,500);
+		//创建一个矩形区域
+		Rect rect = new Rect(0,0, 790,790);
+		//获取上层图片的矩形区域
 		Rect rectF = new Rect(0, 0, bitmap_in.getWidth(), bitmap_in.getHeight());
 		paint.setAntiAlias(true);
+		//获取点九图
 		NinePatch patch = new NinePatch(bitmap_bg, bitmap_bg.getNinePatchChunk(), null);
+		//将点九图绘制在canvas上
 		patch.draw(canvas, rect);
+		//将上册图片绘制在canvas上
 		paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
 		canvas.drawBitmap(bitmap_in, rectF, rect, paint);
 		return roundConcerImage;
 	}
 	public static Bitmap getShardImage(Bitmap bitmap_bg,Bitmap bitmap_in)
 	{
-		Bitmap roundConcerImage = Bitmap.createBitmap(500,500, Config.ARGB_8888);
+		Bitmap roundConcerImage = Bitmap.createBitmap(800,800, Config.ARGB_8888);
 		Canvas canvas = new Canvas(roundConcerImage);
 		Paint paint = new Paint();
-		Rect rect = new Rect(0,0,500,500);
+		Rect rect = new Rect(0,0,800,800);
 		paint.setAntiAlias(true);
 		NinePatch patch = new NinePatch(bitmap_bg, bitmap_bg.getNinePatchChunk(), null);
 		patch.draw(canvas, rect);
-		Rect rect2 = new Rect(2,2,498,498);
+		//注意：如果设置的宽高大于原图片的宽高，如：800 > 790超出790部分将以其他颜色绘制，如：透明色
+		Rect rect2 = new Rect(10,10,800,800);
 		canvas.drawBitmap(bitmap_in, rect, rect2, paint);
 		return roundConcerImage;
 	}
